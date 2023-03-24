a5098ca9876259d0cea3e9ec2357203546df3c63
Anilrocky <anilkumarac96@gmail.com>
2023-03-24 11:53:24 +0530

    [Anil] Add:Folder Structure Created

---
+++ HashTablesAndFunctions/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HashTablesAndFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Tables And Functions!");
+        }
+    }
+}
