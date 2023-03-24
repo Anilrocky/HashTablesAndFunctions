29a08330ab81a7b83eb207812912a3bb1a525d9b
Anilrocky <anilkumarac96@gmail.com>
2023-03-24 15:36:16 +0530

    [Anil] Add:Hash Table

--- HashTablesAndFunctions/Program.cs
+++ HashTablesAndFunctions/Program.cs
@@ -5,7 +5,17 @@ namespace HashTablesAndFunctions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Hash Tables And Functions!");
+            MyMapNode<string, string> myMapNode = new MyMapNode<string, string>(5);
+            myMapNode.Add("0", "to");
+            myMapNode.Add("1", "be");
+            myMapNode.Add("2", "or");
+            myMapNode.Add("3", "not");
+            myMapNode.Add("4", "to");
+            myMapNode.Add("5", "be");
+            string hash2 = myMapNode.Get("2");
+            Console.WriteLine("2nd hash value: " + hash2);
+            string hash5 = myMapNode.Get("5");
+            Console.WriteLine("5th hash value: " + hash5);
         }
     }
 }
